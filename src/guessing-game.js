f677d278912fa7dcf58611c0166a86c50c0f116c
Zhansaule Telisheva <jansaule97@mail.ru>
2023-02-19 19:21:20 +0500

    feat: add guessing game class functionality

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let mid = Math.ceil((this.max + this.min) / 2);
+        return mid;
     }
 
     lower() {
-
+        let maxNum = this.guess();
+        this.setRange(this.min, maxNum);
     }
 
     greater() {
-
+        let minNum = this.guess();
+        this.setRange(minNum, this.max);
     }
 }
 
